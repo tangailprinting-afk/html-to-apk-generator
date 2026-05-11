2e1cd5a8b57213bb9cba4c68f855c0f446db8e6b
Rahim <rahim@gmail.com>
2026-05-11 16:41:58 +0600

    updated dynamic html

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,25 +1,49 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
+import simpleGit from "simple-git";
+
+const git = simpleGit();
 
 export async function POST(req: Request) {
 
   const body = await req.json();
 
-  const { appName, packageName, htmlCode } = body;
+  const { htmlCode } = body;
 
-  const fileName = `${Date.now()}.html`;
+  const indexPath = path.join(
+    process.cwd(),
+    "public",
+    "index.html"
+  );
 
-  const filePath = path.join(process.cwd(), "uploads", fileName);
+  fs.writeFileSync(indexPath, htmlCode);
 
-  fs.writeFileSync(filePath, htmlCode);
+  await git.add("./*");
+
+  await git.commit("updated dynamic html");
+
+  await git.push("origin", "main");
+
+  await fetch(
+    `https://api.github.com/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/actions/workflows/android.yml/dispatches`,
+    {
+      method: "POST",
+
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Accept: "application/vnd.github+json",
+      },
+
+      body: JSON.stringify({
+        ref: "main",
+      }),
+    }
+  );
 
   return NextResponse.json({
     success: true,
-    message: "HTML File Saved Successfully",
-    file: fileName,
-    appName,
-    packageName,
+    message: "Dynamic APK Build Started",
   });
 
 }
