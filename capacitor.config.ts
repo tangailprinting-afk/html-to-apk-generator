edf8e165dea65271af04dc34788a8b00d145ff33
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-12 13:09:34 +0600

    updated capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,9 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.naim.app",
-  appName: "Naim Lyrics",
- webDir: "public",
+  appId: "com.habib.app",
+  appName: "habib",
+  webDir: "public",
 };
 
 export default config;
