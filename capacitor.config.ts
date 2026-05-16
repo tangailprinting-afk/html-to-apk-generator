89c4a7c44f39daa788196e8700ef9715857a87bb
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-16 17:00:31 +0600

    updated capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.rabbisr.app",
+  appId: "com.rabbir.app",
   appName: "rabbiq",
   webDir: "public",
 
