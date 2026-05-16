0304370e3862ca9a785789a8521f40f80526985b
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-16 15:20:51 +0600

    updated capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.jdhfksdf.app",
-  appName: "jdhfksdf",
+  appId: "com.habibmiah.app",
+  appName: "habibmiah",
   webDir: "public",
 
   plugins: {
